e4b836e5a12c01dacca99df1485f3da5d8f8114d
Jacob Hartzell <jacobhartzell@gmail.com>
2025-10-12 11:31:07 -0400

    Got most of it implemented. Trying actions to make sure it passes.

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -6,32 +6,32 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     int fd;
 
     if ( 3 != argc )
     {
        printf("Usage: writer fliename string\n");
-       return;
+       return 1;
     }
 
-    fd = creat(argv[1], O_CREAT | O_TRUNC);
+    fd = creat(argv[1], 0644); 
 
     if (fd == -1)
     {
 	    printf("error opening file\n");
-	    return;
+	    return 1;
     }
 
 
     openlog (NULL,0,LOG_USER); 
     syslog (LOG_ERR,"Writing %s to %s", argv[2], argv[1]);
 
- //    write(fd, argv[2], strlen(argv[2]));
-//    write(fd, "\n", 1);
+    write(fd, argv[2], strlen(argv[2]));
+    write(fd, "\n", 1);
 
     close(fd);
     closelog();
-
+    return 0;
 }
